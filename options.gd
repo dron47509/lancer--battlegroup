b61cb9abf7216494e8438580635f3faa6d71599b
Andrey <dron47509@gmail.com>
2025-08-12 14:58:15 +0500

    Мелкие правки

--- options.gd
+++ options.gd
@@ -3,11 +3,11 @@ extends HBoxContainer
 
 
 func _ready() -> void:
-        for x in get_children():
-                if x.get_child_count() > 1:
-                        if x.get_child(1).text == "0":
-                                x.visible = false
-                        else:
-                                x.visible = true
-                else:
-                        push_warning("%s has no second child; skipping" % x.name)
+	for x in get_children():
+		if x.get_child_count() > 1:
+			if x.get_child(1).text == "0":
+				x.visible = false
+			else:
+				x.visible = true
+		else:
+				push_warning("%s has no second child; skipping" % x.name)
